33470599e7c24761297840ae9c2b9b52d7f0942b
Alejandro Jesus Garcia Covarrubias <alejandro.garcia6126@alumnos.udg.mx>
2025-05-15 17:37:53 -0600

    Hands-on 5

---
+++ Hands-on 5/exec.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() 
+{
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        execlp("ls", "ls", "-l", NULL);
+        perror("Error al ejecutar ls");
+    } else {
+        wait(NULL);
+        printf("Proceso padre esperando al hijo.\n");
+    }
+
+    return 0;
+}
